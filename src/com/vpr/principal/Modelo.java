4a38e06b58760730fe4e0d6480a2cbbfc2288039
vpadillaramos <v.padillaramos@gmail.com>
2018-11-21 21:20:56 +0100

    Comenzado la estructura del modelo

--- src/com/vpr/principal/Modelo.java
+++ src/com/vpr/principal/Modelo.java
@@ -1,7 +1,30 @@
 package com.vpr.principal;
 
+import com.vpr.pojo.Pelicula;
+
 public class Modelo {
+	
+	//Atributos
+	
+	//Constructor
 	public Modelo() {
+		
+	}
+	
+	//Metodos
+	public void anadirPelicula(Pelicula pelicula) {
+		
+	}
+	
+	public void borrarPelicula(Pelicula pelicula) {
+		
+	}
+	
+	public void modificarPelicual(Pelicula pelicula) {
+		
+	}
+	
+	public void serializaHM() {
 		
 	}
 }
--- src/com/vpr/principal/Vista.java
+++ src/com/vpr/principal/Vista.java
@@ -4,16 +4,43 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import java.awt.Font;
+import javax.swing.*;
 
 public class Vista extends JFrame {
 	//Componentes
+	//Panel
 	JPanel panel = new JPanel();
-	public JLabel lbTitulo;
+	
+	//Etiquetas
+	public JLabel lbTitulo = new JLabel("Título");
+	public JLabel lbSinopsis = new JLabel("Título");
+	public JLabel lbGenero = new JLabel("Título");
+	public JLabel lbRating = new JLabel("Título");
+	public JLabel lbDirector = new JLabel("Título");
+	public JLabel lbFechaEstreno = new JLabel("Título");
+	public JLabel lbFehcaVista = new JLabel("Título");
+	public JLabel lbDuracion = new JLabel("Título");
+	public JLabel lbPortada = new JLabel("Título");
+	public JLabel lbImagenes = new JLabel("Título");
+	public JLabel lbNota = new JLabel("Título");
+	public JLabel lbNotas = new JLabel("Título");
+	
+	//Chekbox
+	public JCheckBox chbVista = new JCheckBox("Vista");
+	
+	//Campos
+	public JTextField tfTitulo = new JTextField();
+	public JTextField tfDirector = new JTextField();
+	public JTextField tfDuracion = new JTextField();
+	public JTextField tfNota = new JTextField();
+	
+	//Desplegables
+	public JComboBox cbGenero = new JComboBox();
+	public JComboBox cbRating = new JComboBox();
+	
+	//areas
+	public JTextArea taSinopsis = new JTextArea();
+	public JTextArea taNotas = new JTextArea();
 	
 	
 	public Vista() {
@@ -22,14 +49,12 @@ public class Vista extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
 		setLocationRelativeTo(null);
+		SpringLayout spl = new SpringLayout();
 		
+		//Adicion de componentes
+		add(panel);
 		panel.setBackground(Color.BLACK);
-		panel.setLayout(null);
-		
-		lbTitulo = new JLabel("Título");
-		lbTitulo.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lbTitulo.setForeground(Color.WHITE);
-		panel.add(lbTitulo);
+		panel.setLayout(spl);
 		
 		setVisible(true);
 		repaint();
